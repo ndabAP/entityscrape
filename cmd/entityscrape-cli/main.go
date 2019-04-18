8eb1818fd76d369077b8d7f84d26cec264af340f
Julian Claus <kontakt@julian-claus.de>
2019-04-18 09:33:34 +0200

    fix: ignore .env loading errors

--- cmd/entityscrape-cli/main.go
+++ cmd/entityscrape-cli/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
+	godotenv.Load()
 
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
