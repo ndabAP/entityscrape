e37a409a358196b1053eb68a6162250149ad6204
Julian Claus <kontakt@julian-claus.de>
2019-04-18 09:27:34 +0200

    fix: load environmental variables

--- cmd/entityscrape-cli/main.go
+++ cmd/entityscrape-cli/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "os"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
