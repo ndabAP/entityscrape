a3d205af7e9c67c5d84fa113e4549f78679f5f8a
Julian Claus <kontakt@julian-claus.de>
2019-11-16 09:31:51 +0100

    chore: readd production files

---
+++ website/precache-manifest.50cb1be1cd07364dd70f205fbd0ea596.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d98fa5355df1d8d5882",
+    "url": "/js/app.e1f104f3.js"
+  },
+  {
+    "revision": "87aca88f86ab873d8135",
+    "url": "/css/chunk-vendors.a385397c.css"
+  },
+  {
+    "revision": "87aca88f86ab873d8135",
+    "url": "/js/chunk-vendors.e623e749.js"
+  },
+  {
+    "revision": "2d3718b314aa1a135f2a016ca4216315",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
