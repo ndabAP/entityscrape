dd56108e3f7d006c758d1f1e8fc72aca9a5b7b72
Julian Claus <kontakt@julian-claus.de>
2022-11-07 07:40:29 +0100

    wip: news

--- main.go
+++ main.go
@@ -17,8 +17,6 @@ func init() {
 	log.SetFlags(0)
 }
 
-var logger = log.Default()
-
 func init() {
 	flag.Parse()
 }
@@ -28,63 +26,49 @@ var (
 )
 
 func main() {
-	records := parseRecords("./data/records.csv")
+	articles := parseArticles("./data/articles.csv")
 	entities := parseEntities("./data/entities.csv")
 
-	// TEST
-	records = records[0:2]
+	// TEST START
+	articles = articles[0:2]
+	// TEST END
 
-	resChan := make(chan map[tokenize.Token]float64)
 	ctx := context.Background()
-	go processData(ctx, records, entities, resChan)
+	assocArticles := processArticles(ctx, articles, entities)
+	log.Println(assocArticles)
+}
 
-	i := 0
-	// Per record/news
-	for res := range resChan {
-		if len(res) > 0 {
-			writeResult("./public/assocentities_"+fmt.Sprint(i)+".csv", res)
-			i++
+func processArticles(ctx context.Context, articles [][]string, entities [][]string) map[tokenize.Token]float64 {
+	var (
+		assocArticlesAccum = make(map[tokenize.Token][]float64)
+		assocArticles      = make(map[tokenize.Token]float64)
+	)
+	// For Trump, Putin, Obama
+	for _, entities := range entities {
+		for _, article := range articles {
+			eachText(article, func(text string) {
+				switch text {
+				case "":
+					return
+				}
+
+				assocArticle, err := assocEntities(ctx, text, entities, tokenize.ANY)
+				if err != nil {
+					logAndFail(err)
+				}
+				for tok := range assocArticle {
+					if dist, ok := assocArticlesAccum[tok]; ok {
+						assocArticlesAccum[tok] = append(assocArticlesAccum[tok], dist...)
+					}
+				}
+			})
 		}
 	}
-}
 
-func parseRecords(path string) (data [][]string) {
-	data, err := readCSV(path)
-	if err != nil {
-		logAndFail(err)
+	for tok, dist := range assocArticlesAccum {
+		assocArticles[tok] = avgFloat(dist)
 	}
-	// Remove header
-	data = data[1:]
-	return
-}
-
-func parseEntities(path string) (entities [][]string) {
-	entities, err := readCSV(path)
-	if err != nil {
-		logAndFail(err)
-	}
-	return
-}
-
-func processData(ctx context.Context, records [][]string, entities [][]string, resCh chan map[tokenize.Token]float64) {
-	defer func() {
-		close(resCh)
-	}()
-	for _, entities := range entities {
-		eachText(records, func(text string) {
-			switch text {
-			case "":
-				return
-			}
-
-			res, err := assocEntities(ctx, text, entities, tokenize.ANY)
-			if err != nil {
-				logAndFail(err)
-			}
-
-			resCh <- res
-		})
-	}
+	return assocArticles
 }
 
 func writeResult(path string, res map[tokenize.Token]float64) {
@@ -106,29 +90,47 @@ func writeResult(path string, res map[tokenize.Token]float64) {
 	}
 }
 
-func eachText(records [][]string, textHandler func(content string)) {
-	for _, record := range records {
-		for idx, field := range record {
-			switch idx {
-			case
-				// article_id
-				0,
-				// publish_date
-				1,
-				// article_source_link
-				2,
-				// title
-				3,
-				// subtitle
-				4:
-				continue
+// eachText iterates through articles and call textHandler func on every
+// text containing column, which is in the current case at column 6
+func eachText(article []string, textHandler func(content string)) {
+	for idx, field := range article {
+		switch idx {
+		case
+			// article_id
+			0,
+			// publish_date
+			1,
+			// article_source_link
+			2,
+			// title
+			3,
+			// subtitle
+			4:
+			continue
 
-			// Text
-			case 5:
-				textHandler(field)
-			}
+		// Text
+		case 5:
+			textHandler(field)
 		}
 	}
+}
+
+func parseArticles(path string) (articles [][]string) {
+	articles, err := readCSV(path)
+	if err != nil {
+		logAndFail(err)
+	}
+	// Remove header
+	articles = articles[1:]
+	return
+}
+
+func parseEntities(path string) (entities [][]string) {
+	entities, err := readCSV(path)
+	if err != nil {
+		logAndFail(err)
+	}
+	return
 }
 
 func assocEntities(ctx context.Context, text string, entities []string, pos tokenize.PoS) (assocEntities map[tokenize.Token]float64, err error) {
@@ -154,5 +156,14 @@ func readCSV(path string) (records [][]string, err error) {
 }
 
 func logAndFail(err error) {
-	logger.Fatal(err)
+	log.Fatal(err)
+}
+
+// Returns the average of a float slice
+func avgFloat(xs []float64) float64 {
+	sum := 0.0
+	for _, x := range xs {
+		sum += x
+	}
+	return sum / float64(len(xs))
 }
