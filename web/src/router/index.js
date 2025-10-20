e496f08f952f0aca7cc68fdb70d4cfc381a10869
Julian Claus <kontakt@julian-claus.de>
2025-10-20 10:42:43 +0200

    fix: web client

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/entityscrape/'),
   routes: [
     {
       path: '/',
